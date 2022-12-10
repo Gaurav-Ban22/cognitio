f2adeff6857a5b706f0e19a03fff618171d97e6f
SpellCore <69488672+Gaurav-Ban22@users.noreply.github.com>
2022-12-10 18:21:23 +0000

    basics of restapi not even done

--- server/main.go
+++ server/main.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/googleapis/enterprise-certificate-proxy/client"
+
+	"context"
+	"fmt"
+
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
+
+	"google.golang.org/api/option"
 )
 
+func addCard(c *gin.Context){
+
+
+}
+
+func getCard(c *gin.Context) {
+
+
+}
+
+func getSet(c *gin.Context) {
+
+
+}
+
+func addSet(c *gin.Context) {
+
+	
+}
+
 func main() {
+
+	opt := option.WithCredentialsFile("./ServiceAccountKey.json")
+  	app, err := firebase.NewApp(context.Background(), nil, opt)
+	client, err := app.Firestore(context.Background());
+	if err != nil {
+		log.Fatalln(err);
+
+	}
+	defer client.Close();
 	// Set the router as the default one shipped with Gin
 	router := gin.Default()
 
@@ -24,3 +63,5 @@ func main() {
 
 	router.Run(":3000")
 }
+  
+  
